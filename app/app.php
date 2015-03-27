4fdb3965ce4472add9aaa03d0aaadad7bde4151a
Kyle <student@epicodus.com>
2015-03-27 12:23:34 -0700

    Add home.twig and store_list.twig

---
+++ app/app.php
@@ -0,0 +1,38 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $DB = new PDO('pgsql:host=localhost;dbname=shoes');
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    //home page
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.twig');
+    });
+
+    //all stores view
+    $app->get("/all_stores", function() use ($app) {
+        $stores = Store::getAll();
+        return $app['twig']->render('store_list.twig', array('stores' => $stores));
+    });
+
+    $app->post("/create_store", function() use ($app) {
+        $name = $_POST['store'];
+        $new_store = new Store($name);
+        $new_store->save();
+        $stores = Store::getAll();
+        return $app['twig']->render('store_list.twig', array('stores' => $stores));
+    });
+
+    return $app;
+?>
