a081a498f65e28048ae924b001dd0289e26d7e08
Kyle <student@epicodus.com>
2015-03-27 13:35:15 -0700

    Add brand.twig

--- app/app.php
+++ app/app.php
@@ -26,6 +26,7 @@
         return $app['twig']->render('store_list.twig', array('stores'=>$stores));
     });
 
+    //create a store
     $app->post("/create_store", function() use ($app) {
         $name = $_POST['store'];
         $new_store = new Store($name);
@@ -34,11 +35,72 @@
         return $app['twig']->render('store_list.twig', array('stores'=>$stores));
     });
 
+    //single store view
     $app->get("/store/{id}", function($id) use ($app) {
         $store = Store::find($id);
         $brands = $store->getBrands();
         return $app['twig']->render('store.twig', array('store'=>$store, 'brands'=>$brands));
     });
+
+    //Add a brand to a particular store, from that stores page
+    $app->post("/store/{id}/add_brand", function($id) use ($app) {
+        $store = Store::find($id);
+        $name = $_POST['brand'];
+        $new_brand = new Brand($name);
+        $new_brand->save();
+        $store->addBrand($new_brand);
+        $brands = $store->getBrands();
+        return $app['twig']->render('store.twig', array('store'=>$store, 'brands'=>$brands));
+    });
+
+    //view single brand
+    $app->get("/brand/{id}", function($id) use ($app) {
+        $brand = Brand::find($id);
+        $stores = $brand->getStores();
+        return $app['twig']->render('brand.twig', array('stores'=>$stores, 'brand'=>$brand));
+    });
+
+    //add a new store listing in a particular brand page
+    $app->post("/brand/{id}/add_store", function($id) use ($app) {
+        $brand = Brand::find($id);
+        $store = $_POST['store'];
+        $new_store = new Store($store);
+        $brand->addStore($new_store);
+        $stores = $brand->getStores();
+        return $app['twig']->render('brand.twig', array('stores'=>$stores, 'brand'=>$brand));
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return $app;
 ?>
